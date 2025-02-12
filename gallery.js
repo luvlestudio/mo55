237af3783f33336bc611223b851d57e13d946f39
luvlestudio <sowoojoo31@gmail.com>
2025-02-12 15:55:01 +0900

    55

--- gallery.js
+++ gallery.js
@@ -47,7 +47,7 @@ window.onload = () => {
       objectType: "feed",
       content: {
         title: "이동주 ♥ 김다솜 저희 결혼합니다.",
-        description: "2025년 5월 17일 토요일♡",
+        description: "2025년 5월 17일 (토)\n 오후 12시 40분 MJ컨벤션 파티오볼륨",
         imageUrl: "https://i.imgur.com/srfGsaf.png",
         imageWidth: 1200,
         imageHeight: 630,
